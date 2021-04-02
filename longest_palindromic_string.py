5b7315c70426d8e0da2993e20553995ed897763d
Nick Rossi <rossi_nick@live.co.uk>
2021-04-02 11:30:58 +0100

    Added another .py file

---
+++ longest_palindromic_string.py
@@ -0,0 +1,25 @@
+# find longest palindrome
+
+'''
+Psuedo code for implemented algorithm:
+
+empty_array = []
+for letter in string:
+    letter.append(empty_array)
+    return all combinations in empty_array
+
+palindromes = []
+if combination in empty_array is palindrome:
+    palindromes.append(combination)
+else:
+    pass
+
+return palindromes.max()
+
+'''
+
+
+
+def longest_palindrome(string):
+    pass
+
