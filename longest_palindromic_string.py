b4fb2975fa90d725336d005b65e613bb9a0ae675
Nick Rossi <rossi_nick@live.co.uk>
2021-04-02 11:41:22 +0100

    Altered longest_palindrome file
    Coded first bit of algorithm and tested

--- longest_palindromic_string.py
+++ longest_palindromic_string.py
@@ -5,7 +5,7 @@ Psuedo code for implemented algorithm:
 
 empty_array = []
 for letter in string:
-    letter.append(empty_array)
+    letter.append(empty_array)                      *recursive function to find combinations?*
     return all combinations in empty_array
 
 palindromes = []
@@ -18,8 +18,10 @@ return palindromes.max()
 
 '''
 
+from itertools import combinations as c
 
+# Test for first part of algorithm
+def longest_palindrome(x):
+    return [''.join(l) for i in range(len(x)) for l in c(x, i+1)]
 
-def longest_palindrome(string):
-    pass
-
+print(longest_palindrome('abc'))
